5172c4767eb828f207e739250c6662a870ca24ed
user <joshmanuel2016@gmail.com>
2023-09-11 19:09:11 +0800

    feat: add spinner on home page

--- js/script.js
+++ js/script.js
@@ -43,13 +43,25 @@ async function fetchAPIData(endpoint) {
   const API_KEY = "88a07dbcd1fe74401acd86c1c62b2f10";
   const API_URL = "https://api.themoviedb.org/3/";
 
+  showSpinner();
+
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
 
   const data = await response.json();
 
+  hideSpinner();
+
   return data;
+}
+
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
 }
 
 // Highlight active link
